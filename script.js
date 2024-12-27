02343dc9b69f778079231be66ec6544c73d8226b
David-dev365 <Daviditodo08@gmail.com>
2024-12-27 18:17:42 +0100

    made button to change text color

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-document.getElementById("show").innerHTML = "Hello Javascript!"
-document.getElementById("lock").innerHTML = "Hello CSS!"
+// const word = document.getElementById("show").innerHTML = "Hello Javascript!"
+const word = document.getElementById("show")
+
+const colors = [ 'red', 'blue','crimson', 'orange']
+
+function changeColor() {
+    const index = Math.floor(Math.random() * colors.length)
+
+    word.style.color = `${colors[index]}`
+    // return{index}
+    document.getElementById("range").innerHTML = `${index}`
+
+    
+}
+
+// const {index} =changeColor()
+// document.getElementById("range").innerHTML = `${index}`
+
+changeColor();
+
+
+
+
+
+
+
+const paint = document.getElementById("lock")
+
+
+function changeStyle() {
+    paint.style.color = "green"
+}
+ 
+// const document
